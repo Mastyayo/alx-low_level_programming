87694a13902791119bf8759ca5f7dd5825470aec
feyisara ayodotun <feyisaraayodotun@gmail.com>
2022-09-10 02:26:54 -0700

    Patience, persistence and perspiration

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -17,8 +17,9 @@ int main(void)
 		putchar(d);
 		if (d != '9')
 		{
-			putchar(',');															putchar(' ');														}
-	}		
+			putchar(',');															putchar(' ');
+		}
+	}
 	putchar('\n');
 
 	return (0);
