b67c63d74375ce2b4bd1c4448ccd496d7afdfd6e
feyisara ayodotun <feyisaraayodotun@gmail.com>
2022-09-22 15:41:02 -0700

    paranoid in reverse

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -7,14 +7,30 @@
  */
 void reverse_array(int *a, int n)
 {
-	int temp, counter;
+	int *start_c, *end_c, c;
+	int i;
 
-	n = n - 1;
-	counter = 0;
-	while (counter <= n)
+	/*Start_c and end_c is equal value of variable s*/
+	start_c = a;
+	end_c = a;
+
+	/*Move the end_c to the last character*/
+	for (i = 0; i < n - 1; i++)
 	{
-		temp = a[counter];
-		a[counter++] = a[n];
-		a[n--] = temp;
+		end_c++;
+	}
+
+	/* Swap the char from start and end */
+	/* index using begin_ptr and end_ptr */
+	for (i = 0; i < n / 2; i++)
+	{
+		/*swap character*/
+		c = *end_c;
+		*end_c = *start_c;
+		*start_c = c;
+
+		/*update pointers positions*/
+		start_c++;
+		end_c--;
 	}
 }
