dba4c162b5f2c8f93aa108533359bb819ae277c2
feyisara ayodotun <feyisaraayodotun@gmail.com>
2022-09-22 22:45:52 -0700

    Noise is a buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -12,6 +12,7 @@
 void print_line(char *c, int s, int l)
 {
 	int j, k;
+
 	for (j = 0; j <= 9; j++)
 	{
 	if (j <= s)
@@ -40,6 +41,7 @@ void print_line(char *c, int s, int l)
 void print_buffer(char *b, int size)
 {
 	int i;
+
 	for (i = 0; i <= (size - 1) / 10 && size; i++)
 	{
 		printf("%08x: ", i * 10);
@@ -56,5 +58,3 @@ void print_buffer(char *b, int size)
 	if (size == 0)
 		putchar('\n');
 }
-		
-	
