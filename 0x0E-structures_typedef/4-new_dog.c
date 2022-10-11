7a9324b5d251faaccf0abfbf5f418b43f9665e46
feyisara ayodotun <feyisaraayodotun@gmail.com>
2022-10-10 22:55:40 -0700

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,49 +2,50 @@
 #include "dog.h"
 /**
  * new_dog - create a new dog
- * @name: name of dog
- * @age: age of dog
- * @owner: owner of dog
+ * @name: 1st member
+ * @age: 2nd member
+ * @owner: 3rd member
  *
- * Return: struct dog, if fail returns NULL
+ * Return: NULL if fail
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *p_dog;
-	int i, dname, downer;
+	int i, lenN, lenO;
 
-	p_dog = malloc(sizeof(*p_dog));
-	p_dog = malloc(sizeof(*p_dog));
+	struct dog *n_dog = NULL;
+
+	lenN = 0;
+	while (name[lenN] != '\0')
+		lenN++;
+	lenO = 0;
+	while (name[lenO] != '\0')
+		lenO++;
+
+	n_dog = malloc(sizeof(struct dog));
+	if (n_dog == NULL)
 	{
-		free(p_dog);
+		free(n_dog);
 		return (NULL);
 	}
-
-	for (dname = 0; name[dname]; dname++)
-		;
-	for (downer = 0; owner[downer]; downer++)
-		;
-
-	p_dog->name = malloc(dname + 1);
-	p_dog->owner = malloc(downer + 1);
-
-	if (!(p_dog->name) || !(p_dog->owner))
+	n_dog->name = malloc(lenN + 1);
+	if (n_dog->name == NULL)
 	{
-		free(p_dog->name);
-		free(p_dog->owner);
-		free(p_dog);
+		free(n_dog->name);
+		free(n_dog);
 		return (NULL);
 	}
-
-	for (i = 0; i < dname; i++)
-		p_dog->name[i] = name[i];
-	p_dog->name[i] = '\0';
-
-	p_dog->age = age;
-
-	for (i = 0; i < downer; i++)
-		p_dog->owner[i] = owner[i];
-	p_dog->owner[i] = '\0';
-
-	return (p_dog);
+	n_dog->owner = malloc(lenO + 1);
+	if (n_dog->owner == NULL)
+	{
+		free(n_dog->name);
+		free(n_dog->owner);
+		free(n_dog);
+		return (NULL);
+	}
+	for (i = 0; i <= lenN; i++)
+		n_dog->name[i] = name[i];
+	for (i = 0; i <= lenO; i++)
+		n_dog->owner[i] = owner[i];
+	n_dog->age = age;
+	return (n_dog);
 }
