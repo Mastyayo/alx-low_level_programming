c2d13affe112c96275005475e648fbc230991cc2
feyisara ayodotun <feyisaraayodotun@gmail.com>
2022-10-12 16:14:19 -0700

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+/**
+ * print_name - print a name
+ * @name: input of name
+ * @f: function pointer
+ *
+ * Return: no return
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
